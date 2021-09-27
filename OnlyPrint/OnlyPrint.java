fd68d90e6ae5027a7146625a51432a20ac01460f
Michelle Bloodworth <83024708+SlimBloodworth@users.noreply.github.com>
2021-09-27 12:59:43 -0500

    Add files via upload

---
+++ OnlyPrint/OnlyPrint.java
@@ -0,0 +1,10 @@
+public class OnlyPrint
+{
+	public static void main(String []args)
+	{
+		System.out.print("It is \"really\"\n\n");
+		System.out.print("hard to skip\n\n");
+		System.out.print("lines without using\n\n");
+		System.out.print("println.");
+		}
+	}
